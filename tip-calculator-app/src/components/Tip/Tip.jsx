997f6a97ddedc3e090095e381830472b2c6b0fe1
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-05-22 23:36:42 +0530

    tips buttons check

--- tip-calculator-app/src/components/Tip/Tip.jsx
+++ tip-calculator-app/src/components/Tip/Tip.jsx
@@ -1,14 +1,36 @@
 import './Tip.css'
 
 const Tip = () => {
+  const percentages = [5, 10, 15, 25, 50]
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div className="tip">
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <label htmlFor="billInput">Bill</label>
         <input type="number" id="billInput" />
-        <label htmlFor="selectTip">select tip %</label>
-        5% 10% 15% 25% 50%
-        <input type="number" name="" id="selectTip" placeholder='custom' />
+        <label>select tip %</label>
+        <div className="tipPercents" id="selectTip">
+          {percentages.map((percent, index) => {
+            return (
+              <button
+                className="percentTip"
+                key={index}
+              >{percent}%</button>
+            )
+          })}
+
+          <input
+            type="number"
+            name="customTip"
+            id="selectTip"
+            placeholder="custom"
+            className="customTip"
+          />
+        </div>
         <label htmlFor="numberOfPeople">number of people</label>
         <input type="number" name="numberOfPeople" id="numberOfPeople" />
       </form>
