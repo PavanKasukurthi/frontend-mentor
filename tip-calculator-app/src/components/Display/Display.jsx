ba347c644ed2340b75fc19a1ebcedad0a3a991e8
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-05-23 00:17:45 +0530

    container design update

--- tip-calculator-app/src/components/Display/Display.jsx
+++ tip-calculator-app/src/components/Display/Display.jsx
@@ -2,14 +2,24 @@ import './Display.css'
 
 const Display = () => {
   return (
-    <div className='display'>
+    <div className="display">
       <div className="tipAmount">
-        <p>Tip Amount</p>
-        <p>/ person</p>
+        <div className="tipAmount-left">
+          <p>Tip Amount</p>
+          <p>/ person</p>
+        </div>
+        <div className="tipAmount-right">
+          <h1>$0.00</h1>
+        </div>
       </div>
       <div className="total">
-        <p>total</p>
-        <p>/ person</p>
+        <div className="total-left">
+          <p>total</p>
+          <p>/ person</p>
+        </div>
+        <div className="total-right">
+          <h1>$0.00</h1>
+        </div>
       </div>
       <button className="reset-btn">RESET</button>
     </div>
--- tip-calculator-app/src/components/Tip/Tip.jsx
+++ tip-calculator-app/src/components/Tip/Tip.jsx
@@ -1,8 +1,6 @@
 import './Tip.css'
 
 const Tip = () => {
-  const percentages = [5, 10, 15, 25, 50]
-
   const handleSubmit = (e) => {
     e.preventDefault()
   }
@@ -10,29 +8,41 @@ const Tip = () => {
   return (
     <div className="tip">
       <form onSubmit={handleSubmit}>
-        <label htmlFor="billInput">Bill</label>
-        <input type="number" id="billInput" />
-        <label>select tip %</label>
-        <div className="tipPercents" id="selectTip">
-          {percentages.map((percent, index) => {
-            return (
-              <button
-                className="percentTip"
-                key={index}
-              >{percent}%</button>
-            )
-          })}
+        <label className="subHeading" htmlFor="billInput">
+          Bill
+        </label>
+        <input className="billInput" type="number" id="billInput" />
 
-          <input
-            type="number"
-            name="customTip"
-            id="selectTip"
-            placeholder="custom"
-            className="customTip"
-          />
+        <p className="subHeading">Select Tip %</p>
+        {/* <label>select tip %</label> */}
+        <div class="button-group">
+          <button id="5" class="percent-btn" type="button">
+            5%
+          </button>
+          <button id="10" class="percent-btn" type="button">
+            10%
+          </button>
+          <button id="15" class="percent-btn" type="button">
+            15%
+          </button>
+          <button id="25" class="percent-btn" type="button">
+            25%
+          </button>
+          <button id="50" class="percent-btn" type="button">
+            50%
+          </button>
+          <input id="percent" type="text" placeholder="Custom" />
         </div>
-        <label htmlFor="numberOfPeople">number of people</label>
-        <input type="number" name="numberOfPeople" id="numberOfPeople" />
+
+        <label className="subHeading" htmlFor="numberOfPeople">
+          number of people
+        </label>
+        <input
+          className="numberOfPeople"
+          type="number"
+          name="numberOfPeople"
+          id="numberOfPeople"
+        />
       </form>
     </div>
   )
